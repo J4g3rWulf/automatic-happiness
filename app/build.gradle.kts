84f9518783ae46de523e0cd9e3df38c0848070d1
Gianluca Paz <gianlucapaz@gmail.com>
2026-03-23 00:49:15 -0300

    chore: corrige applicationId para br.recycleapp no build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.br.recycleapp"
+        applicationId = "br.recycleapp"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
